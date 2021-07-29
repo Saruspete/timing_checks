79ba3a8ca76235f63384da9cabaa175d41545b33
Adrien Mahieux <adrien.mahieux@gmail.com>
2021-07-30 01:28:17 +0200

    [simple/gettimeofday] Add more stats

--- simple/gettimeofday.c
+++ simple/gettimeofday.c
@@ -1,14 +1,13 @@
-#include <sys/time.h>
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <sys/time.h>
+#include <sched.h>
 
 int main(int argc, char* argv[]) {
 
-	int threshold = 3;
-	int loops = 10000000;
-
-	printf("Called: %s with %d args\n", argv[0], argc);
+	int threshold = 5;
+	int loops = 100000000;
 
 	switch (argc) {
 		case 2:
@@ -20,9 +19,20 @@ int main(int argc, char* argv[]) {
 			break;
 	}
 
-	size_t i=1;
-	struct timeval  tv[2];
+	size_t i=1, overCnt=0, overMax=0;
+	struct timeval  tv[2], tvStart, tvStop;
 
+	gettimeofday(&tvStart, NULL);
+
+	unsigned int cpuNum, nodeNum;
+	getcpu(&cpuNum, &nodeNum);
+
+	printf("Start: %ld.%ld\n", tvStart.tv_sec, tvStart.tv_usec);
+	printf("CPU: on cpu %u node %u\n", cpuNum, nodeNum);
+	printf("Samples: Taking %d samples\n", loops);
+
+	// First iteration to avoid useless tests
+	gettimeofday(&tv[0], NULL);
 
 	while (i < loops) {
 		gettimeofday(&tv[i%2], NULL);
@@ -32,10 +42,24 @@ int main(int argc, char* argv[]) {
 
 		if (diff >= threshold) {
 			printf("%9.9ld time: %ld.%ld  Diff: %d\n", i, tv[i%2].tv_sec, tv[i%2].tv_usec, diff );
+			overCnt++;
+			if (diff > overMax)
+				overMax = diff;
 		}
 
 		i++;
 	}
+
+	gettimeofday(&tvStop, NULL);
+	int diff =
+		( tvStop.tv_sec  - tvStart.tv_sec ) * 1000000 +
+		( tvStop.tv_usec - tvStart.tv_usec);
+	getcpu(&cpuNum, &nodeNum);
+
+	printf("End: %ld.%ld\n", tvStop.tv_sec, tvStop.tv_usec);
+	printf("CPU: on %u node %u\n", cpuNum, nodeNum);
+	printf("Samples: in %f sec (%ld sample/sec)  got %ld over-threshold (max: %ld)\n",
+		 (float)diff/1000000, i*1000000/diff, overCnt, overMax);
 
 	return 0;
 }
